9cd88a5bd096b562d66584b58d5e2cedc74b3288
XavierC003 <xaviercollege3@gmail.com>
2025-03-31 16:11:47 -0400

    Allocated block edit

--- src/memory_manager/memory_block/allocated_block.rs
+++ src/memory_manager/memory_block/allocated_block.rs
@@ -1,4 +1,7 @@
 // you'll probably want to expand this...
+use crate::memory_manager::memory_block::MemoryBlock;
+
+
 pub struct AllocatedBlock {
     start: usize,   // starting address of the block
     end: usize,     // ending address of the block
@@ -10,5 +13,15 @@ pub struct AllocatedBlock {
 impl AllocatedBlock {
     pub fn new(start:usize, size: usize, id: String, used_size: usize) -> Self {
         Self { start, size, id, used_size}
+    }
+}
+
+impl MemoryBlock for AllocatedBlock {
+    fn get_start(&self) -> usize {
+        self.start
+    }
+
+    fn get_size(&self) -> usize {
+        self.size
     }
 }
