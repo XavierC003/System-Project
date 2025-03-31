acc723a906c577a7c52475988e17d1772b32010c
XavierC003 <xaviercollege3@gmail.com>
2025-03-31 16:06:36 -0400

    Free_block

--- src/memory_manager/memory_block/free_block.rs
+++ src/memory_manager/memory_block/free_block.rs
@@ -3,3 +3,27 @@ pub struct FreeBlock {
     start: usize,
     size: usize,
 }
+
+impl FreeBlock {
+    pub fn new(start: usize, size: usize) -> Self {
+        Self { start, size }
+    }
+
+    // Check if blocks are adjacent
+    pub fn is_adjacent(&self, other: &FreeBlock) -> bool {
+        self.start + self.size == other.start || other.start + other.size == self.start
+    }
+
+    // Merge two adjacent blocks
+    pub fn merge(&self, other: &FreeBlock) -> Option<FreeBlock> {
+        if self.is_adjacent(other) {
+            let new_start = self.start.min(other.start);
+            let new_size = self.size + other.size;
+            Some(FreeBlock::new(new_start, new_size))
+        }
+        else {
+            None  // Return None if they are not adjacent
+        }
+    }
+    
+}
