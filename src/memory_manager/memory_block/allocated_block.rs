ecc5adf3edbbab3a11acd202e37389f56e6d33de
XavierC003 <xaviercollege3@gmail.com>
2025-03-31 15:59:00 -0400

    Allocated block

--- src/memory_manager/memory_block/allocated_block.rs
+++ src/memory_manager/memory_block/allocated_block.rs
@@ -1,6 +1,14 @@
 // you'll probably want to expand this...
 pub struct AllocatedBlock {
-    start: usize,
-    size: usize,
-    id: String,
+    start: usize,   // starting address of the block
+    end: usize,     // ending address of the block
+    size: usize,    // total size of the block
+    id: String,    // unique identifier for the block
+    used_size: usize,   // size of the block that is currently used
+}
+
+impl AllocatedBlock {
+    pub fn new(start:usize, size: usize, id: String, used_size: usize) -> Self {
+        Self { start, size, id, used_size}
+    }
 }
