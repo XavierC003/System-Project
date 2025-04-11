dd7ae6b86574c3034c88e542178cdcce5c1defe5
XavierC003 <xaviercollege3@gmail.com>
2025-04-11 00:05:55 -0400

    Fixed delete function

--- src/memory_manager/delete.rs
+++ src/memory_manager/delete.rs
@@ -1,17 +1,20 @@
-use crate::memory_manager::merge_free_blocks;
 use crate::memory_block::FreeBlock;
 use super::MemoryManager;
 
 pub fn delete(manager: &mut MemoryManager, id: &str) -> bool {
-    if let Some(index) = manager.allocated_blocks.iter().position(|b| b.id == id) {
-        let block = manager.allocated_blocks.remove(index);
-        manager.free_blocks.push(FreeBlock::new(block.start, block.size));
-        merge_free_blocks(manager);
+    // Find the block with the specified id in allocated_handles
+    if let Some(index) = manager.allocated_handles.iter().position(|b| b.id == id) {
+        // Remove the block from allocated_handles
+        let block = manager.allocated_handles.remove(index);
+        
+        // Add the freed block to free_handles
+        manager.free_handles.push(FreeBlock::new(block.get_start(), block.get_size()));
+        
+        println!("Deleted block with id: {}", id);
         return true;
     }
+    // Return false if no block was found with the given id
+    println!("No block found with id: {}", id);
     false
 }
 
-pub fn print_block_info<T: memory_block::MemoryBlock>(block: &T) {
-    println!("Start: {}, Size: {}", block.get_start(), block.get_size());
-}
